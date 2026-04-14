a4f5d73fd0a4affbe8abb99824cd24be54d385b3
Ak3ss <167654630+Ak3ss@users.noreply.github.com>
2026-04-14 17:59:37 +0200

    Update txnoclip.lua

--- txnoclip.lua
+++ txnoclip.lua
@@ -1,14 +1,1 @@
-local noclipEnabled = false
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if IsControlJustPressed(0, 73) then
-            noclipEnabled = not noclipEnabled
-            if noclipEnabled then
-                TriggerEvent('txcl:setPlayerMode', "noclip", true)
-            else
-                TriggerEvent('txcl:setPlayerMode', "none", true)
-            end
-        end
-    end
-end)
+print("hey there")
